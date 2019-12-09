4f314095afa647f18b8fed58a98f405daef0d2b4
Roseakhonya <akhonyarose@gmail.com>
2019-12-09 14:25:47 +0300

    add find profile function

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,12 +7,21 @@ import { Profile } from 'selenium-webdriver/firefox';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  private profile: any[];
+  private profile: "";
+  private repos:"";
+  private username: "string";
 
   constructor(private profileService: ProfileService) { 
+  }
+    findProfile(){
+      this.profileService.updateProfile(this.username);
     this.profileService.getProfileInfo().subscribe(profile =>{
       console.log(profile);
-      this.profile = profile;
+      this.profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos =>{
+      console.log(this.repos);
+      this.repos;
     });
   }
 
