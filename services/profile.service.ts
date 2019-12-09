1c3ba5a75392eb8b1e4a6ed3cbb48204cfeb59e6
Roseakhonya <akhonyarose@gmail.com>
2019-12-09 09:02:30 +0300

    add get function

--- services/profile.service.ts
+++ services/profile.service.ts
@@ -18,6 +18,8 @@ export class ProfileService {
 
    }
    getProfileInfo() {
-     return this.http.get("https://api.github.com/users/") + this.username + "?client_id=" +
+     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + 
+     this.clientid + "&client_secret=" + this.clientsecret)
+     .map(res=>res.json());
    }
 }
