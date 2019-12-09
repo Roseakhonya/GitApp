182bec66466c380870903d6ea743e2c94c7208fc
Roseakhonya <akhonyarose@gmail.com>
2019-12-09 10:47:54 +0300

    add constructor

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'services/profile.service';
+import { Profile } from 'selenium-webdriver/firefox';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  private profile: any[];
 
   constructor(private profileService: ProfileService) { 
     this.profileService.getProfileInfo().subscribe(profile =>{
-      console.log(profile)
+      console.log(profile);
+      this.profile = profile;
     });
   }
 
